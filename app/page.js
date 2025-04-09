6e13218bc8139256be6c41fc4c204ac1aa293e7d
mahammedjunedattar <123448134+mahammedjunedattar@users.noreply.github.com>
2025-04-09 00:37:55 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -61,7 +61,7 @@ const handleSignUp = async (e) => {
   const handleSignIn = async (e) => {
     e.preventDefault();
     try {
-        const response = await fetch('/apis/auth/Signup', {
+        const response = await fetch('/apis/auth/signin', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password })
